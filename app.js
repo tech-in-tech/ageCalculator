14647c2e972349da91db0dcf0619380904c2be3d
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-02-26 07:29:11 +0530

    fix some bugs

--- app.js
+++ app.js
@@ -7,40 +7,40 @@ let day = document.querySelector('#days');
 // console.log(userInput.value);
 
 
-btn.addEventListener("click",()=>{
+btn.addEventListener("click", () => {
   // !birth date
   let birthDate = new Date(userInput.value)
   let d = birthDate.getDate();
-  let m = birthDate.getMonth()+1;
+  let m = birthDate.getMonth() + 1;
   let y = birthDate.getFullYear();
   console.log(`${d}-${m}-${y}`);
-  
+
   //! today's date
   let today = new Date();
   let d1 = today.getDate();
-  let m1 = today.getMonth()+1;
+  let m1 = today.getMonth() + 1;
   let y1 = today.getFullYear();
   console.log(`${d1}-${m1}-${y1}`);
-  
-  let ad,am,ay;
-  ay = y1-y;
+
+  let ad, am, ay;
+  ay = y1 - y;
   // * calculate days
-  if(d1<d){
+  if (d1 < d) {
     m1--;
-    d1+=30;
-    ad = d1-d;
+    d1 += 30;
+    ad = d1 - d;
   }
-  else{
-    ad = d1-d;
+  else {
+    ad = d1 - d;
   }
-// * calculate months;
-  if(m1>=m){
-    am = m1-m;
+  // * calculate months;
+  if (m1 >= m) {
+    am = m1 - m;
   }
-  else{
-    y1--;
-    m1+=12;
-    am = m1-m;
+  else {
+    ay--;
+    m1 += 12;
+    am = m1 - m;
   }
   years.innerText = ay;
   days.innerText = ad;
